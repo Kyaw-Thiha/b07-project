3ab275755c0ef103e102f37412a362114f129f7e
najjalowe <najjaalowe@gmail.com>
2025-11-26 13:02:52 -0500

    feat: further expanded technique helper

--- app/src/main/java/com/example/b07project/TechniqueHelperActivity.java
+++ app/src/main/java/com/example/b07project/TechniqueHelperActivity.java
@@ -1,25 +1,71 @@
 package com.example.b07project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.b07project.view.child.ChildDashboardActivity;
 import com.example.b07project.view.common.BackButtonActivity;
 
 public class TechniqueHelperActivity extends BackButtonActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_technique_helper);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    EdgeToEdge.enable(this);
+    setContentView(R.layout.activity_technique_helper);
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+      Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+      return insets;
+    });
+    TextView stepTwo = findViewById(R.id.textView61);
+    TextView stepThree = findViewById(R.id.textView65);
+    TextView stepTwoInhaler = findViewById(R.id.textView62);
+    TextView stepTwoMask = findViewById(R.id.textView64);
+    TextView stepThreeMask = findViewById(R.id.textView66);
+
+    stepTwo.setVisibility(View.INVISIBLE);
+    stepThree.setVisibility(View.INVISIBLE);
+    stepTwoInhaler.setVisibility(View.INVISIBLE);
+    stepTwoMask.setVisibility(View.INVISIBLE);
+    stepThreeMask.setVisibility(View.INVISIBLE);
+  }
+
+  public void inhaler(View view) {
+    TextView stepTwo = findViewById(R.id.textView61);
+    TextView stepThree = findViewById(R.id.textView65);
+    TextView stepTwoInhaler = findViewById(R.id.textView62);
+    TextView stepTwoMask = findViewById(R.id.textView64);
+    TextView stepThreeMask = findViewById(R.id.textView66);
+    stepTwo.setVisibility(View.VISIBLE);
+    stepTwoInhaler.setVisibility(View.VISIBLE);
+    stepThree.setVisibility(View.INVISIBLE);
+    stepTwoMask.setVisibility(View.INVISIBLE);
+    stepThreeMask.setVisibility(View.INVISIBLE);
+  }
+
+  public void inhalerMask(View view) {
+    TextView stepTwo = findViewById(R.id.textView61);
+    TextView stepThree = findViewById(R.id.textView65);
+    TextView stepTwoInhaler = findViewById(R.id.textView62);
+    TextView stepTwoMask = findViewById(R.id.textView64);
+    TextView stepThreeMask = findViewById(R.id.textView66);
+    stepTwo.setVisibility(View.VISIBLE);
+    stepTwoInhaler.setVisibility(View.INVISIBLE);
+    stepThree.setVisibility(View.VISIBLE);
+    stepTwoMask.setVisibility(View.VISIBLE);
+    stepThreeMask.setVisibility(View.VISIBLE);
+  }
+
+  public void techniqueTwo(View view) {
+    Intent intent = new Intent(this, SecondTechniqueHelperActivity.class);
+    startActivity(intent);
+  }
 }
--- app/src/main/java/com/example/b07project/ThirdTechniqueHelperActivity.java
+++ app/src/main/java/com/example/b07project/ThirdTechniqueHelperActivity.java
@@ -1,22 +1,25 @@
-package com.example.b07project.view.child;
+package com.example.b07project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.example.b07project.R;
 
+import com.example.b07project.view.child.ChildDashboardActivity;
 import com.example.b07project.view.common.BackButtonActivity;
 
-public class ChildSettingsActivity extends BackButtonActivity {
+public class ThirdTechniqueHelperActivity extends BackButtonActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_child_settings);
+        setContentView(R.layout.activity_third_technique_helper);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,5 +27,8 @@ public class ChildSettingsActivity extends BackButtonActivity {
         });
     }
 
-
+    public void finish(View view){
+        Intent intent = new Intent(this, ChildDashboardActivity.class);
+        startActivity(intent);
+    }
 }
