eeaeae4efa338c104ce06ffb1ce048e05104b00f
Kevin_KyawThiha <k.thiha10.mail@gmail.com>
2025-11-27 17:04:50 -0500

    Updated the date format

--- app/src/main/java/com/example/b07project/TestAPIActivity.java
+++ app/src/main/java/com/example/b07project/TestAPIActivity.java
@@ -129,7 +129,7 @@ public class TestAPIActivity extends AppCompatActivity {
       CheckIn.NightWalking nightWalking = new CheckIn.NightWalking(false, triggers, "nothing");
       CheckIn checkIn = new CheckIn(now, nightWalking, null, null, childUid);
 
-      Medicine med = new Medicine("stuffs", "11/20/25", "01/26/26", 200, "01/26/26", parentUid);
+            Medicine med = new Medicine("2025-11-20", "2026-01-26", 0.63, 200, "2026-01-26", parentUid);
       MedicineLog medicineLog = new MedicineLog(now, 2, "worse", "better", childUid);
       PEF pef = new PEF(now, 250, 300, childUid);
 
