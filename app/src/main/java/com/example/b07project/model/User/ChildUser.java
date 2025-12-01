c206da25e3367d81b25db0b1bf8f2f391d400d12
Kevin_KyawThiha <k.thiha10.mail@gmail.com>
2025-12-01 13:33:25 -0500

    Added notes for child user

--- app/src/main/java/com/example/b07project/model/User/ChildUser.java
+++ app/src/main/java/com/example/b07project/model/User/ChildUser.java
@@ -5,30 +5,52 @@ import com.example.b07project.model.*;
 import java.util.Map;
 
 public class ChildUser extends User {
-    private Boolean ageBelow9;
-    private String parentId;
+  private Boolean ageBelow9;
+  private String dateOfBirth;
+  private String parentNotes;
+  private String parentId;
 
-    public ChildUser(){
-    }
-    public ChildUser(String uid, String name, String email, Map<String, Boolean> roles, Boolean ageBelow9, String parentId) {
-        super(uid, name, email, roles);
-        this.ageBelow9 = ageBelow9;
-        this.parentId = parentId;
-    }
+  public ChildUser() {
+  }
 
-    public Boolean isAgeBelow9() {
-        return ageBelow9;
-    }
+  public ChildUser(String uid, String name, String email, Map<String, Boolean> roles, Boolean ageBelow9,
+      String dateOfBirth, String parentNotes, String parentId) {
+    super(uid, name, email, roles);
+    this.ageBelow9 = ageBelow9;
+    this.dateOfBirth = dateOfBirth;
+    this.parentNotes = parentNotes;
+    this.parentId = parentId;
+  }
 
-    public void setAgeBelow9(Boolean ageBelow9) {
-        this.ageBelow9 = ageBelow9;
-    }
+  public Boolean getIsAgeBelow9() {
+    return ageBelow9;
+  }
 
-    public String getParentId() {
-        return parentId;
-    }
+  public void setAgeBelow9(Boolean ageBelow9) {
+    this.ageBelow9 = ageBelow9;
+  }
 
-    public void setParentId(String parentId) {
-        this.parentId = parentId;
-    }
+  public String getDateOfBirth() {
+    return this.dateOfBirth;
+  }
+
+  public void setDateOfBirth(String dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public String getParentNotes() {
+    return this.parentNotes;
+  }
+
+  public void setParentNotes(String parentNotes) {
+    this.parentNotes = parentNotes;
+  }
+
+  public String getParentId() {
+    return parentId;
+  }
+
+  public void setParentId(String parentId) {
+    this.parentId = parentId;
+  }
 }
