e51920e9942cfd55bdb3c2f3dd54af6196c69059
2192375d <173829746yzpq@gmail.com>
2025-12-02 21:00:09 -0500

    fix: unboarding going back doesn't start again

--- app/src/main/java/com/example/b07project/view/parent/ParentOnboardingPage2Activity.java
+++ app/src/main/java/com/example/b07project/view/parent/ParentOnboardingPage2Activity.java
@@ -24,6 +24,7 @@ public class ParentOnboardingPage2Activity extends BackButtonActivity {
                         ParentOnboardingPage3Activity.class
                 );
                 startActivity(intent);
+                finish();
             }
         });
     }
